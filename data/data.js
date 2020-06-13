9eaa5f9169313b7c6e716c29f2520dc25e567ff4
Pere <pere.solaclaver@gmail.com>
2020-06-13 18:26:27 +0100

    add new answers

--- data/data.js
+++ data/data.js
@@ -1,25 +1,70 @@
 const questions = {
-	senior: {
-		1: {
+	senior: [
+		{
+			id: 1,
 			pregunta: 'Xicots a la Diada de Sant Felix?',
 			respostes: ['si', 'no'],
-			correcte: 'si',
+			correcte: 'no',
 		},
-		2: {
+		{
+			id: 2,
 			pregunta: 'Com es deia el Carrer de Cal Bolet abans?',
-			respostes: [
-				'Carrer de Duc de la Victoria',
-				'el gegant del pi',
-				'ai mare neu a missa',
-			],
-			correcte: 'Carrer de Duc de la Victoria',
+			respostes: ['C/ Duc de la Victoria', 'C/ Melió', 'C/ Ponent'],
+			correcte: 'C/ Duc de la Victoria',
 		},
-		3: {
+		{
+			id: 3,
 			pregunta: "Quin dia d'agost comencen els Goigs de St. Felix?",
-			respostes: [21, 22, 23],
-			correcte: 21,
+			respostes: ['21', '22', '23'],
+			correcte: '21',
 		},
-	},
+		{
+			id: 4,
+			pregunta: 'On estava el Discostar??',
+			respostes: ['Pl. St. Jaume', 'C/ Cort', 'La Parellada'],
+			correcte: 'La Parellada',
+		},
+		{
+			id: 5,
+			pregunta: 'Com es deia el Casal Okupa de la Crta Vilanova?',
+			respostes: ['Kasal Alternatiu', 'Can Cellerot', 'Ni fu ni fa'],
+			correcte: 'Can Cellerot',
+		},
+		{
+			id: 6,
+			pregunta: 'Qui patrocinava la matinal esportiva de Radio Vilafranca?',
+			respostes: ['Mobles Guardiet', 'Mobles Quer', 'Mobles Feliu'],
+			correcte: 'Mobles Feliu',
+		},
+		{
+			id: 7,
+			pregunta: 'Què son els Murris?',
+			respostes: [
+				"La Canalla d'una colla castellera",
+				'Fans del FC. Vilafranca',
+				'Personatges de Caixa Penedes per a nens',
+			],
+			correcte: 'Personatges de Caixa Penedes per a nens',
+		},
+		{
+			id: 8,
+			pregunta: 'Quin dia comencen les matines de la Festa Major?',
+			respostes: ['29', '30', '31'],
+			correcte: '30',
+		},
+		{
+			id: 9,
+			pregunta: "Quin dia es canta l'escurçó negre?",
+			respostes: ["La nit de cap d'any", "El 30 d'agost", "L'11 de setembre"],
+			correcte: "El 30 d'agost",
+		},
+		{
+			id: 10,
+			pregunta: 'Com es deia la Pl. Penedès abans?',
+			respostes: ['Pl. Exèrcit', 'Pl. Pere III', 'Pl. Vilanova'],
+			correcte: 'Pl. Exèrcit',
+		},
+	],
 };
 
 module.exports = questions;
