6956ae33442286c417da176773fd18356556965b
Pere <pere.solaclaver@gmail.com>
2020-07-09 23:11:20 +0100

    Add fallback in case lenght of questions is more than 10. Add returning error message.

--- api/controller/questions.js
+++ api/controller/questions.js
@@ -1,25 +1,36 @@
 // data
-const data = require('../../data/data');
+const questions = require('../../data/data');
 const novetats = require('../../data/novetats');
 
-function fetchQuestions(req, res) {
-	const { age } = req.body;
-	// focus on the difficulty of questions
-	const originalQuestions = data[age];
-	// initialize array that will be returned with the 10 random questions
-	const fetchedQuestions = [];
+async function fetchQuestions(req, res) {
+	try {
+		const { age } = req.body;
+		// focus on the difficulty of questions
+		const originalQuestions = questions[age];
+		// initialize array that will be returned with the 10 random questions
+		let fetchedQuestions = [];
 
-	const takenItems = {};
-	// get 10 random questions
-	for (let i = 0; i < 10; i++) {
-		let idx = Math.floor(Math.random() * originalQuestions.length);
-		while (takenItems[idx]) {
-			idx = Math.floor(Math.random() * originalQuestions.length);
+		const takenItems = {};
+		// get 10 random questions
+		for (let i = 0; i < 10; i++) {
+			let idx = Math.floor(Math.random() * originalQuestions.length);
+			while (takenItems[idx]) {
+				idx = Math.floor(Math.random() * originalQuestions.length);
+			}
+			fetchedQuestions.push(originalQuestions[idx]);
+			takenItems[idx] = true;
 		}
-		fetchedQuestions.push(originalQuestions[idx]);
-		takenItems[idx] = true;
+		// fall back in case, for some reason, the returned array of questions is > 10
+		if (fetchedQuestions.length > 10) {
+			fetchedQuestions = [];
+			for (let i = 0; i < 10; i++) {
+				fetchedQuestions.push(originalQuestions[i]);
+			}
+		}
+		res.status(200).json({ data: fetchedQuestions });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
 	}
-	res.status(200).json({ data: fetchedQuestions });
 }
 
 function fetchNovetats(req, res) {
